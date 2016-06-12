34cc2b3351f751309fc37d26f528b4c0493e7885
Alonzo Wahl <aawahl@gmail.com>
2016-06-12 10:23:03 -0400

    refactor a bit

--- spec/lib/birdv/fb_helper_spec.rb
+++ spec/lib/birdv/fb_helper_spec.rb
@@ -15,44 +15,49 @@ stub_request(:post, "www.example.com").
 
 describe 'FBHelper' do
 
-	let (:fb_caller) { Class.new{ include Birdv::FBHelper }.new }
-
+	# facebook url, i.e. 'https://graph.facebook.com/v2.6/me/messages?access_token=blahblahblah'
 	FB_URI  = "#{Birdv::FBHelper.get_graph_url}?access_token=#{ENV['FB_ACCESS_TKN']}"
+
+	# stub HTTP responses from facebook
 	FAILURE = [{"error":{"message":"(#100) Invalid fbid.","type":"OAuthException","code":100,"fbtrace_id":"A0Nh+OHr+TX"}}, 400]
 	SUCCESS = [{"recipient_id":"10209571935726081","message_id":"mid.1465592244050:7b2b9006d900596e93"}, 200]
+
 	DUMMY_MSG = ["some FB ID", "this is a generic message"]
 
+	# a mock instance of a class that has FBHelper functions
+	let (:fb_caller) { Class.new{ include Birdv::FBHelper }.new }
+
+	# a reusable wrapper for #stub_request from webmock
 	let (:stub_response ) do 
 		lambda do |response, code| 
 			stub_request(:any, FB_URI).to_return(body: response.to_s, status: code)
-				#to_return(body: "abc", status: 400, headers: { 'Content-Length' => 3 })
 		end
 	end	
 
 	context 'text message fails' do
-		before(:example) do 
-			stub_response.call(*FAILURE)
-		end
+			before(:example) do 
+				stub_response.call(*FAILURE)
+			end
 
-		it 'returns the FB error code' do
-			ret = fb_caller.send_txt(*DUMMY_MSG)
-			expect(ret.body['error']).not_to be_empty
-		end
+			it 'returns the FB error code' do
+				ret = fb_caller.send_txt(*DUMMY_MSG)
+				expect(ret.body['error']).not_to eq(nil)
+			end
 	end
 
 	context 'text message succeeds' do
-		before(:example) do 
-			stub_response.call(*SUCCESS)
-		end
+			before(:example) do 
+				stub_response.call(*SUCCESS)
+			end
 
-		it 'does not return FB error code' do
-			ret = fb_caller.send_txt(*DUMMY_MSG)
-			expect(ret.body['error']).to eq(nil)
-		end
+			it 'does not return FB error code' do
+				ret = fb_caller.send_txt(*DUMMY_MSG)
+				expect(ret.body['error']).to eq(nil)
+			end
 
-		it 'has a recipient_id' do
-			ret = fb_caller.send_txt(*DUMMY_MSG)
-			expect(ret.body['recipient_id']).not_to be_empty
-		end
+			it 'has a recipient_id' do
+				ret = fb_caller.send_txt(*DUMMY_MSG)
+				expect(ret.body['recipient_id']).not_to eq(nil)
+			end
 	end
 end
--- spec/lib/birdv/workers/story_courier_spec.rb
+++ spec/lib/birdv/workers/story_courier_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 require 'birdv/workers/story_courier'
 
 describe StoryCourier do
-  context 'when FB throws error on third page' do
-    before do
 
-    end
-  end
+	# test #perfrom behavior
+  context 'poop'
 
+	end
+  # 
 end
