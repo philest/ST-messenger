e63e91ff45fa2d8457b5883f463e415ce97de636
Phil Esterman <phil.esterman@yale.edu>
2016-06-15 18:44:56 -0400

    integrated the Cook title and ending into 10 page story; changed send_story to index from 1 instead of 0

--- bin/register_bot.rb
+++ bin/register_bot.rb
@@ -13,7 +13,7 @@ response = HTTParty.post(
 	"https://graph.facebook.com/v2.6/me/subscribed_apps",
 	query: {
 		access_token: ENV["FB_ACCESS_TKN"]
-	},
+	}
 	# header: {
 	# 	'X-Hub-Signature': generate_hmac(tbody)
 	# }
