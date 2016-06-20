f19cf868085656b9df26471d22beb66921804fe6
Phil Esterman <phil.esterman@yale.edu>
2016-06-20 16:09:25 -0400

    false start on teacher associations

--- lib/app/app.rb
+++ lib/app/app.rb
@@ -15,34 +15,15 @@ get '/' do
  'hello world'
 end
 
-get '/enroll' do 
-	
-	# register the users, with names if possible
-	25.times do |idx|
-		if params["\"phone_#{idx}\""]
-			user = User.create(phone: params["phone_#{idx}"])
-			if params["name_#{idx}"]
-				user.update(name: params["name_#{idx}"])
-			end
-		end
-	end	
-
-	#TODO hook the users to their teacher
-
-end
 
 
 post '/enroll' do
 	
-	'hello world'
 
-	puts params
-
-	require 'pry'
-	binding.pry
-
+	# Create the parents
 	25.times do |idx|
 		if params["phone_#{idx}"]
+
 			user = User.create(phone: params["phone_#{idx}"])
 			if params["name_#{idx}"]
 				user.update(name: params["name_#{idx}"])
@@ -51,10 +32,6 @@ post '/enroll' do
 	end	
 
 	puts User.count
-
-	# user = User.create(:name => param, :phone => param1)
-	# teacher = Teacher.create(:name => name, :email => email, :school => school)
-	# user.teacher = teacher
 
 
 
