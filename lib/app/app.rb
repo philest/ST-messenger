5c73a0b963ac6c7ad298fd7cd9b11496b0de280f
Phil Esterman <phil.esterman@yale.edu>
2016-06-20 17:26:09 -0400

    /enroll receives POST request and properly enrolls

--- lib/app/app.rb
+++ lib/app/app.rb
@@ -22,29 +22,40 @@ post '/enroll' do
 	# that's a later challenge.
 
 	# TODO : what teacher info will we have?
-	if defined? params["teacher_signature"]
-				# create a new teacher with a phone number
-				signature =  params["teacher_prefix"] + params["teacher_signature"]
-				puts "creating new teacher: #{signature}"
-				teacher = Teacher.create(:signature => signature)
+	if params["teacher_signature"] != nil
+		# create a new teacher with a phone number
+		if params["teacher_prefix"] == ''
+			signature = params["teacher_signature"]
+		else
+			signature =  params["teacher_prefix"] + " " + params["teacher_signature"]
+		end
+
+		begin
+			teacher = Teacher.create(:signature => signature)
+			puts "created new teacher: #{signature}"
+		rescue Sequel::Error => e
+			p e.message
+		end
 	end
 
 	# Create the parents
 	25.times do |idx|
-		if params["phone_#{idx}"] != ''
+		if params["phone_#{idx}"] != nil
 
 			begin 
-			parent = User.create(:phone =>  params["phone_#{idx}"])
-	    parent.update(:name => params["name_#{idx}"]) if params["name_#{idx}"] != ''
-	    rescue Sequel::UniqueConstraintViolation => e
-	      p e.message << " ::> did not insert, already exists in db"
-	    rescue Sequel::Error => e
-	      p e.message << " ::> failure"
+				parent = User.create(:phone =>  params["phone_#{idx}"])
+			    parent.update(:name => params["name_#{idx}"]) if params["name_#{idx}"] != nil
+
+			    if defined? teacher
+					teacher.add_user(parent)
+				end
+		    rescue Sequel::UniqueConstraintViolation => e
+		      	p e.message << " ::> did not insert, already exists in db"
+		    rescue Sequel::Error => e
+		     	p e.message << " ::> failure"
 			end
 
-			if defined? teacher
-				teacher.add_user(parent)
-			end
+
 		end
 	end	
 	
