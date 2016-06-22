5669412d8363e85a2d9298fda1875f9b5a5d443c
jdmcpeek <david.mcpeek@yale.edu>
2016-06-22 18:16:45 -0400

    update registration

--- lib/bot/intro.rb
+++ lib/bot/intro.rb
@@ -22,29 +22,28 @@ def demo(recipient)
       "We hope you love these."
     )
 
-  	# save user in the database.
+  # save user in the database.
+  # TODO : update an existing DB entry to coincide the fb_id with phone_number
+  begin
+    fields = "first_name,last_name,profile_pic,locale,timezone,gender"
+    data = HTTParty.get("https://graph.facebook.com/v2.6/#{recipient['id']}?fields=#{fields}&access_token=#{ENV['FB_ACCESS_TKN']}")
+    name = data["first_name"] + " " + data["last_name"]
+  rescue
+    User.create(:fb_id => recipient["id"])
+  else
+    puts "successfully found user data for #{name}"
+    last_name = data['last_name']
+    regex = /[a-zA-Z]*( )?#{last_name}/i  # if child's last name matches, go for it
     begin
-      users = DB[:users] 
-      begin
-        fb_name = HTTParty.get("https://graph.facebook.com/v2.6/#{recipient['id']}?fields=first_name,last_name&access_token=#{ENV['FB_ACCESS_TKN']}")
-        name = fb_name["first_name"] + " " + fb_name["last_name"]
-      rescue HTTParty::Error
-        name = ""
+      candidates = User.where(:child_name => regex, :fb_id => nil)
+      if candidates.all.empty? # add a new user w/o child info (no matches)
+        User.create(:fb_id => recipient['id'], :name => name, :gender => data['gender'], :locale => data['locale'], :profile_pic => data['profile_pic'])
       else
-        puts "successfully found name"
-      end
-
-      begin 
-        users.insert(:name => name, :fb_id => recipient["id"])
-        puts "inserted #{name}:#{recipient} into the users table"
-      rescue Sequel::UniqueConstraintViolation => e
-        p e.message
-        puts "did not insert, already exists in db"
-      rescue Sequel::Error => e
-        p e.message
-        puts "failure"
+        # implement stupid fb_name matching to existing user matching
+        candidates.order(:enrolled_on).first.update(:fb_id => recipient['id'], :name => name, :gender => data['gender'], :locale => data['locale'], :profile_pic => data['profile_pic'])
       end
     rescue Sequel::Error => e
-      p e.message
-    end
+      p e.message + " did not insert, already exists in db"
+    end # rescue - db transaction
+  end # rescue - httparty
 end
