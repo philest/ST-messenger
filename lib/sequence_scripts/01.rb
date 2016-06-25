58dd739bb4754eb0d61f25b73cf32e15239cdde8
jdmcpeek <david.mcpeek@yale.edu>
2016-06-25 14:44:48 -0400

    fixed merge conflicts

--- lib/sequence_scripts/01.rb
+++ lib/sequence_scripts/01.rb
@@ -33,15 +33,8 @@ Birdv::DSL::StoryTimeScript.new 'day1' do
 		# send out cook story
 		img_1 = "https://s3.amazonaws.com/st-messenger/day1/tap_and_swipe.jpg"
 		send picture(img_1), recipient
-<<<<<<< HEAD
-		send_story 'day1', 'coon', 9, recipient, 15
-=======
-		send_story 'day1', 'coon', 8, recipient
 
-		img_2 = "https://s3.amazonaws.com/st-messenger/day1/go_up.jpg"
-		send picture(img_2), recipient, 23
->>>>>>> 087b9fd450d635d5764cdd5abcc6565ddb7dc262
-		
+		send_story 'day1', 'coon', 9, recipient, 15
 
 		# one more button
 		send button('thanks'), recipient
