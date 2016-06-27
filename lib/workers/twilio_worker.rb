6df5ef78a2e7c726c153945be641b4d7dfa3a4b2
jdmcpeek <david.mcpeek@yale.edu>
2016-06-27 11:34:58 -0400

    twilio worker does nothing

--- lib/workers/twilio_worker.rb
+++ lib/workers/twilio_worker.rb
@@ -9,15 +9,15 @@ class TwilioWorker
 
 
 	def perform(name, number, teacher)
-		client = Twilio::REST::Client.new ENV["TW_ACCOUNT_SID"], ENV["TW_AUTH_TOKEN"]
-		from = "+12032023505" # Your Twilio number
-		body = "Hi, this is #{teacher}. I've signed up our class to get free nightly books on StoryTime. Just click here:\njoinstorytime.com/books"
-		client.account.messages.create(
-			:from => from,
-			:to => number,
-			:body => body
-		)
-		puts "Sent message to parent of #{name}"
+		# client = Twilio::REST::Client.new ENV["TW_ACCOUNT_SID"], ENV["TW_AUTH_TOKEN"]
+		# from = "+12032023505" # Your Twilio number
+		# body = "Hi, this is #{teacher}. I've signed up our class to get free nightly books on StoryTime. Just click here:\njoinstorytime.com/books"
+		# client.account.messages.create(
+		# 	:from => from,
+		# 	:to => number,
+		# 	:body => body
+		# )
+		puts "Sent message to parent of #{name} (but not actually)"
 
 		# update the user day! TODO: make this a seperate job!
 	end
