25fde2ca16e891c69b43922d27fc6f28a5a0acaf
jdmcpeek <david.mcpeek@yale.edu>
2016-06-30 11:12:54 -0400

    add before_destroy hook to user

--- models/user.rb
+++ models/user.rb
@@ -5,7 +5,23 @@ class User < Sequel::Model(:users)
 	many_to_one :teacher
 	many_to_one :classroom
 	one_to_many :button_press_logs
-	one_to_one :enrollment_queue
+	one_to_one :enrollment_queue 
+
+
+	def before_destroy
+		super
+		# clean your room, mister!
+		ButtonPressLog.where(user_id: self.id).delete
+		EnrollmentQueue.where(user_id: self.id).delete
+	end
+
+	def after_create
+		super
+		eq = EnrollmentQueue.create(user_id: self.id)
+		self.enrollment_queue = eq
+		eq.user = self
+		
+	end
 
 	def validate
     	super
