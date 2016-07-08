db1a2ecd795d586caaf98e5c5b34a781792f4fb0
jdmcpeek <david.mcpeek@yale.edu>
2016-07-08 15:20:58 -0400

    fixed specs

--- bin/user.rb
+++ bin/user.rb
@@ -8,8 +8,12 @@ query = "fields=first_name,last_name,locale,timezone,gender"
 
 # first_name,last_name,locale,timezone,gender
 
-fb_name = JSON.parse HTTParty.get("https://graph.facebook.com/v2.6/#{ENV["DAVID_QUAILTIME"]}?fields=first_name,last_name,gender&access_token=#{ENV['FB_ACCESS_TKN']}").body
+# fb_name = JSON.parse HTTParty.get("https://graph.facebook.com/v2.6/#{ENV["DAVID_QUAILTIME"]}?fields=first_name,last_name,gender&access_token=#{ENV['FB_ACCESS_TKN']}").body
 
-puts fb_name
+puts HTTParty.get("https://graph.facebook.com/v2.6/10209967651611613?access_token=EAAYOZCnHw2EUBAKs6JRf5KZBovzuHecxXBoH2e3R5rxEsWlAf9kPtcBPf22AmfWhxsObZAgn66eWzpZCsIZAcyX7RvCy7DSqJe8NVdfwzlFTZBxuZB0oZCw467jxR89FivW46DdLDMKjcYUt6IjM0TkIHMgYxi744y6ZCGLMbtNteUQZDZD&fields=first_name,last_name,profile_pic,locale,timezone,gender").body.to_json
+
+# fb_name = HTTParty.get("https://graph.facebook.com/v2.6/bad_id?fields=first_name,last_name,gender&access_token=#{ENV['FB_ACCESS_TKN']}").body.to_json
+
+# puts fb_name
 # puts HTTParty.get("#{uri}?#{query}&access_token=#{ENV['FB_ACCESS_TKN']}")
 # 
