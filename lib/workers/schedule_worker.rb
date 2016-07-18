9916d0ad5be7dd1d467869a5fcf0ce78b06b6abb
Phil Esterman <phil.esterman@yale.edu>
2016-07-18 09:49:56 -0400

    Friends don't let friends get ST everyday.

--- lib/workers/schedule_worker.rb
+++ lib/workers/schedule_worker.rb
@@ -65,15 +65,12 @@ class ScheduleWorker
   	# server timein UTC
 		now 			= Time.now.utc.seconds_since_midnight
 
-    # our_friend? =
 
 		# DST-adjusted user time
 		user_local = adjust_tz(user)
 		user_utc	 = user_local.utc.seconds_since_midnight
 		user_day 	 = get_local_day(Time.now, user)
-		if (user_day==1||user_day==3||user_day==5) ||
-      our_friend?(user)
-
+		if (user_day==1||user_day==3||user_day==5)
 			if now >= user_utc
 				now - user_utc <= range
 			else
