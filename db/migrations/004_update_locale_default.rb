a8077ec59344d4839f9738b02ba712871fb4f78d
jdmcpeek <david.mcpeek@yale.edu>
2016-07-19 17:02:08 -0400

    update default story_number from state_table

--- db/migrations/004_update_locale_default.rb
+++ db/migrations/004_update_locale_default.rb
@@ -4,13 +4,28 @@ Sequel.migration do
       drop_column :locale
       add_column :locale, String, default: 'en'
     end
+
+    alter_table(:state_tables) do
+      drop_column :story_number
+      add_column :story_number, Integer, default: 1
+    end
+
+
+
   end
 
   down do
+
+    alter_table(:state_tables) do
+      drop_column :story_number
+      add_column :story_number, Integer, default: 0
+    end
+
     alter_table(:users) do
       drop_column :locale
       add_column :locale, String, default: 'en_US'
     end
+
   end
 
 end
