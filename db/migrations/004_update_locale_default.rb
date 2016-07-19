41bc5cb4aaa47a689f55a684187734753125cceb
jdmcpeek <david.mcpeek@yale.edu>
2016-07-19 16:59:56 -0400

    locale commit

---
+++ db/migrations/004_update_locale_default.rb
@@ -0,0 +1,16 @@
+Sequel.migration do
+  up do
+    alter_table(:users) do
+      drop_column :locale
+      add_column :locale, String, default: 'en'
+    end
+  end
+
+  down do
+    alter_table(:users) do
+      drop_column :locale
+      add_column :locale, String, default: 'en_US'
+    end
+  end
+
+end
