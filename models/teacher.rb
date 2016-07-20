07e001ce6e276ec9912cccfa79696f9163bfbbb5
jdmcpeek <david.mcpeek@yale.edu>
2016-07-20 18:20:38 -0400

    school migration, update model + associations for schools

--- models/teacher.rb
+++ models/teacher.rb
@@ -1,10 +1,13 @@
 class Teacher < Sequel::Model(:teachers)
 	plugin :timestamps, :create=>:enrolled_on, :update=>:updated_at, :update_on_create=>true
 	plugin :validation_helpers
+	plugin :association_dependencies
 
 	many_to_one :school
 	one_to_many :classrooms
 	one_to_many :users
+
+	add_association_dependencies users: :nullify
 
 
 	def validate
@@ -13,14 +16,5 @@ class Teacher < Sequel::Model(:teachers)
     	validates_unique :email, :allow_nil=>true, :message => "email #{email} is already taken (teachers)"
     	validates_unique :fb_id, :allow_nil=>true, :message => "fb_id #{fb_id} is already taken (teachers)"
   	end
-
-	# def validate
- #    	super
- #    	# get users from database
- #    	teachers = DB[:teachers]
- #    	if not teachers.where(:phone => phone).empty?
- #    		errors.add(:phone, "must be unique - phone #{phone} already exists in teachers table")
- #    	end
- #  	end
 
 end
