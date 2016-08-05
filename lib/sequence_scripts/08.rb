87d394c94fa707b530a7c5cffdd49194ed737225
Phil Esterman <phil.esterman@yale.edu>
2016-08-05 22:44:02 +0300

    chores poem script

---
+++ lib/sequence_scripts/08.rb
@@ -0,0 +1,49 @@
+Birdv::DSL::ScriptClient.new_script 'day8' do
+
+	day 6
+	#
+	# register some buttons for reuse!
+	# ================================
+	# NOTE: always call story_button, template_generic, 
+	# and button_normal OUTSIDE of sequence blocks
+	#
+	button_story({
+		name: 		'tap_here',
+		title: 		'scripts.buttons.title',
+		image_url:  'scripts.buttons.story_img_url', 
+		buttons: 	[postback_button('scripts.buttons.tap', script_payload(:storysequence))]
+	})
+
+	button_normal({
+		name: 			 'thanks',
+		window_text: 'scripts.buttons.window_text',
+		buttons: 			[postback_button('scripts.buttons.thanks', script_payload(:yourwelcome))]
+	})
+
+	sequence 'firsttap' do |recipient|
+		# greeting with 4 second delay
+		txt = 'scripts.teacher_intro'
+		send recipient, text({text:txt}),  4 
+		
+		# send tap_here button
+		send recipient, button({name:'tap_here'})
+	end
+
+	sequence 'storysequence' do |recipient|
+		
+		# send out story
+		send recipient, story(), 23
+
+		# delay 'thanks'
+		delay recipient, 'thanks', 23.seconds
+	end
+
+	sequence 'thanks' do |recipient|
+		# one more button
+		send recipient, button({name:'thanks'})
+	end
+
+	sequence 'yourwelcome' do |recipient|
+		send recipient, text({text:'scripts.buttons.welcome'})
+	end
+end 
