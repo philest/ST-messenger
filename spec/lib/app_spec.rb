e85bd0ae2d2dbf6aa87c0d410b9ef77343628021
jdmcpeek <david.mcpeek@yale.edu>
2016-08-15 17:31:23 -0400

    more unit tests in app_spec

--- spec/lib/app_spec.rb
+++ spec/lib/app_spec.rb
@@ -41,6 +41,32 @@ describe SMS do
       }.to change{User.count}.by 1
     end
 
+    context "matching users with schools" do
+      before(:each) do
+        @school = School.create(code: "turkey|turquia", signature: "the Turkey Farm", name: "TurkeyZilla")
+        @school_params = {"ToCountry"=>"US", "ToState"=>"CT", "SmsMessageSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "NumMedia"=>"0", "ToCity"=>"DARIEN", "FromZip"=>"90066", "SmsSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "FromState"=>"CA", "SmsStatus"=>"received", "FromCity"=>"LOS ANGELES", "Body"=>"TURKEY", "FromCountry"=>"US", "To"=>"+12032023505", "ToZip"=>"06820", "NumSegments"=>"1", "MessageSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "AccountSid"=>"ACea17e0bba30660770f62b1e28e126944", "From"=>"+18186897323", "ApiVersion"=>"2010-04-01"}
+        @school_params_espanish = {"ToCountry"=>"US", "ToState"=>"CT", "SmsMessageSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "NumMedia"=>"0", "ToCity"=>"DARIEN", "FromZip"=>"90066", "SmsSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "FromState"=>"CA", "SmsStatus"=>"received", "FromCity"=>"LOS ANGELES", "Body"=>"TURQUIA", "FromCountry"=>"US", "To"=>"+12032023505", "ToZip"=>"06820", "NumSegments"=>"1", "MessageSid"=>"SM3461cd2ebfa515456d2a956c03dee788", "AccountSid"=>"ACea17e0bba30660770f62b1e28e126944", "From"=>"+18186897323", "ApiVersion"=>"2010-04-01"}
+
+      end
+
+      it "matches a user with a school when they've texted in correctly, ignoring case" do 
+        post '/sms', @school_params
+
+        user = User.where(phone: '8186897323').first
+
+        expect(user.school_id).to eq @school.id
+      end
+
+      it "updates a user's language to Spanish when they text the spanish code" do
+        post '/sms', @school_params_espanish
+        user = User.where(phone: '8186897323').first
+
+        expect(user.locale).to eq 'es'
+      end
+
+    end
+
+
     it "enqueues a texting job" do
       Sidekiq::Testing::fake! do
         expect {
@@ -71,6 +97,48 @@ describe SMS do
         }.to change{User.count}.by(0)
       end
     end
+  end
+
+  # test that skips someone without an ID
+  # test the new user does indeed add a field to enrollment queue
+  context 'post /' do
+    before(:example) do
+      post '/', @enroll_params
+    end
+
+    it "processes phone numbers???" do
+    end
+
+    it "adds David, Phil, and Aubrey to the DB" do
+      expect(User.where(phone: @enroll_params[:phone_0], child_name: @enroll_params[:name_0]).first).to_not be_nil
+      expect(User.where(phone: @enroll_params[:phone_1], child_name: @enroll_params[:name_1]).first).to_not be_nil
+      expect(User.where(phone: @enroll_params[:phone_2], child_name: @enroll_params[:name_2]).first).to_not be_nil
+    end
+
+    it "correctly assigns teacher" do
+      users = User.where(phone: [@enroll_params[:phone_0], @enroll_params[:phone_1], @enroll_params[:phone_3]])
+      teacher = Teacher.where(email: @enroll_params[:teacher_email]).first
+      for user in users
+        expect(user.teacher.id).to eq teacher.id
+      end
+    end
+
+    it "only adds one teacher" do 
+      expect(Teacher.count).to eq 1
+    end
+
+    it "updates teacher row when it's the same teacher (same email)" do
+      post '/', { teacher_email: @enroll_params[:teacher_email], name_0: "Ben McPeek", phone_0: "8183210034", teacher_signature: "McEsterWahl" }
+      # previous teacher was updated...
+      expect(Teacher.count).to eq 1
+      # user was inserted...
+      expect(User.where(child_name: "Ben McPeek").first).to_not be_nil
+      # same teacher
+      expect(User.where(child_name: "Ben McPeek").first.teacher).to eq Teacher.where(email: @enroll_params[:teacher_email]).first
+      # there are 4 users now...
+      expect(User.count).to eq 4
+    end
+
   end
 
 
