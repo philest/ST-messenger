e140de466e40c04ed4f571ca279c76b9df742e60
jdmcpeek <david.mcpeek@yale.edu>
2016-08-25 18:44:02 -0400

    remove double button press shit

--- lib/workers/message_worker.rb
+++ lib/workers/message_worker.rb
@@ -57,25 +57,25 @@ class MessageWorker
         b = ButtonPressLog.new(:day_number=>s.script_day, :sequence_name=>sequence)
         u.add_button_press_log(b)
 
-        protected_ids = %w(1084495154927802 1042751019139427 8186897323 1625783961083197 10209967651611613 10209571935726081)
+        # protected_ids = %w(1084495154927802 1042751019139427 8186897323 1625783961083197 10209967651611613 10209571935726081)
         
-        last_sequence_seen = u.state_table.last_sequence_seen
+        # last_sequence_seen = u.state_table.last_sequence_seen
 
-        # check if the sequence request > last sequence seen in DSL ordering
-        sequence_seen      = s.sequence_seen?(sequence, last_sequence_seen)
+        # # check if the sequence request > last sequence seen in DSL ordering
+        # sequence_seen      = s.sequence_seen?(sequence, last_sequence_seen)
 
-                             # user story# \leq current request AND not seen the sequence?
-        numbers_check_out  = u.state_table.story_number <= s.script_day    && !sequence_seen  
+        #                      # user story# \leq current request AND not seen the sequence?
+        # numbers_check_out  = u.state_table.story_number <= s.script_day    && !sequence_seen  
 
 
-        Sidekiq.logger.warn numbers_check_out ? "#{recipient} not yet seen #{sequence}" : "#{recipient} already saw #{sequence}"
+        # Sidekiq.logger.warn numbers_check_out ? "#{recipient} not yet seen #{sequence}" : "#{recipient} already saw #{sequence}"
 
         
         # ...but if they didn't already press the button, send sequence
-        if   numbers_check_out                 \
-          || last_sequence_seen == 'intro'      \
-          || last_sequence_seen == 'teachersend' \
-          || protected_ids.include?(recipient)
+        # if   numbers_check_out                 \
+        #   || last_sequence_seen == 'intro'      \
+        #   || last_sequence_seen == 'teachersend' \
+        #   || protected_ids.include?(recipient)
           # TODO: or query?
 
           # TODO: run this in a worker
@@ -85,9 +85,9 @@ class MessageWorker
           
           # looking for updating user's story#/storyday? well it's 
           # actually a clock worker behavior lol sorry
-        else
-          puts "we've seen this button before..."
-        end
+        # else
+        #   puts "we've seen this button before..."
+        # end
 
       # TODO: do we want an ELSE behavior?
       end
