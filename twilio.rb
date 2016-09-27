41cfcdebb44eef066a9750d78e1b57faf4e3e179
jdmcpeek <david.mcpeek@yale.edu>
2016-09-27 11:02:06 -0400

    update poems with title and ending

--- twilio.rb
+++ twilio.rb
@@ -6,9 +6,9 @@ STORYTIME_NO  = "+12032023505"
 
 client = Twilio::REST::Client.new ENV['TW_ACCOUNT_SID'], ENV['TW_AUTH_TOKEN']
 
-line1 = "The smallest of the baby birds\nWas too afraid to fly.\nHis sisters flew, his brothers too,\nBut baby bird just cried."
+line1 = "BABY BIRD\n\nThe smallest of the baby birds\nWas too afraid to fly.\nHis sisters flew, his brothers too,\nBut baby bird just cried."
 line2 = "His mother said, \"Oh baby bird!\"\n\"You'll do it if you try!\"\n\"I promise you can do it son,\"\n\"A mother never lies!\""
-line3 = "And baby bird trusted his mom,\nSo once he dried his eyes,\nHe bravely leapt out from the nest\nAnd flew into the sky."
+line3 = "And baby bird trusted his mom,\nSo once he dried his eyes,\nHe bravely leapt out from the nest\nAnd flew into the sky.\n\nTHE END"
 
 # @message = client.account.messages.get("M1ea228a51d444121b0f5f94a63daee0a")
 # puts @message.body
@@ -17,8 +17,14 @@ line3 = "And baby bird trusted his mom,\nSo once he dried his eyes,\nHe bravely 
 
 
 client.account.messages.create(
-  body: "Para recibir cuentos por mensaje texto, responde con TEXT",
-  to: "2036687820",
+  body: line1,
+  to: "8186897323",
   from: STORYTIME_NO
 )
 
+
+client.account.messages.create(
+  body: line3,
+  to: "8186897323",
+  from: STORYTIME_NO
+)
