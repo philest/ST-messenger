707292e41e5d465bba68209a136529ac1d3a54a2
jdmcpeek <david.mcpeek@yale.edu>
2016-10-09 17:09:11 -0400

    outro messages

--- lib/sequence_scripts/demo.rb
+++ lib/sequence_scripts/demo.rb
@@ -40,7 +40,7 @@ Birdv::DSL::ScriptClient.new_script 'demo', 'fb' do
   })
 
   sequence 'birdgreeting' do |recipient|
-    txt = "*Here’s tonight’s Storytime story for Jasmine!\n-Ms. Wilson"
+    txt = "*Today in class Jasmine told us all about her baby bird, so I wanted to share this story tonight :) -Ms. Wilson"
     send recipient, text({text:txt})
     send recipient, button({name:'tap_here_bird'})
     unsubscribe_demo recipient
@@ -52,14 +52,19 @@ Birdv::DSL::ScriptClient.new_script 'demo', 'fb' do
   end
 
   sequence 'seedgreeting' do |recipient|
-    txt = "*Here’s another story!"
+    txt = "*Jasmine and the class watered our garden today! Here’s tonight's story about growing a magic tree. -Ms. Wilson"
     send recipient, text({text:txt})
     send recipient, button({name:'tap_here_seed'})
   end
 
   sequence 'seedstorysequence' do |recipient|
     send recipient, story({story: 'seeddemo'})
-    delay recipient, 'helpmessage', 12.seconds
+    delay recipient, 'outromessage', 10.seconds
+  end
+
+  sequence 'outromessage' do |recipient|
+    txt = "*Jasmine has been getting a lot stronger at sounding out letters. This reading is really helping. Thank you mom :)"
+    send recipient, text({text: txt})
   end
 
   sequence 'helpmessage' do |recipient|
