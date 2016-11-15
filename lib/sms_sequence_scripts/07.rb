7717b726670b43a6e3049fb697f97e6d1fd2df7d
jdmcpeek <david.mcpeek@yale.edu>
2016-11-15 17:54:12 -0500

    new sms stories

---
+++ lib/sms_sequence_scripts/07.rb
@@ -0,0 +1,41 @@
+Birdv::DSL::ScriptClient.new_script 'day7', 'sms' do
+
+  # recipients are phone numbers
+  sequence 'firstmessage' do |phone_no|
+    txt = 'scripts.intro_sms.__poc__[1]'
+    puts "sending intro txt..."
+    send_sms phone_no, txt, 'firstmessage', 'image1'
+
+    # delay the conventional SMS delay
+    # delay phone_no, 'image1', SMS_WAIT
+  end
+
+  sequence 'image1' do |phone_no|
+    # send out coon story
+    img = 'mms.stories.baboon[0]'
+    puts "sending first image..."
+    send_mms phone_no, img, 'image1', 'image2'
+
+    # delay phone_no, 'image2', MMS_WAIT
+  end
+
+  # No button on the first day! 
+  sequence 'image2' do |phone_no|
+    # one more button
+    puts "sending second image..."
+    img = 'mms.stories.baboon[1]'
+    send_mms phone_no, img, 'image2', 'goodbye'
+
+    # delay phone_no, 'goodbye', MMS_WAIT
+  end
+
+  sequence 'goodbye' do |phone_no|
+    puts "saying goodbye..."
+
+    txt = 'scripts.outro.__poc__[2]'
+    send_sms phone_no, txt, 'goodbye'
+  end
+
+
+end 
+
