7fed91c5db29a8db81b98509ca2a7e2866df59e6
jdmcpeek <david.mcpeek@yale.edu>
2016-11-22 13:40:55 -0800

    new mms

--- lib/sms_sequence_scripts/09.rb
+++ lib/sms_sequence_scripts/09.rb
@@ -1,15 +1,32 @@
 Birdv::DSL::ScriptClient.new_script 'day9', 'sms' do
+  # recipients are phone numbers
   sequence 'firstmessage' do |phone_no|
-    txt = 'scripts.intro_sms.__poc__[1]'
+    txt = 'scripts.intro_sms.__poc__[2]'
     puts "sending intro txt..."
     send_sms phone_no, txt, 'firstmessage', 'image1'
   end
 
   sequence 'image1' do |phone_no|
-    img = 'mms.stories.moon[0]'
+    img = 'mms.stories.floating_shoe[0]'
     puts "sending first image..."
-    send_mms phone_no, img, 'image1'
+    send_mms phone_no, img, 'image1', 'image2'
+  end
+ 
+  sequence 'image2' do |phone_no|
+    puts "sending second image..."
+    img = 'mms.stories.floating_shoe[1]'
+    send_mms phone_no, img, 'image2', 'goodbye'
+  end
+
+  sequence 'goodbye' do |phone_no|
+    puts "saying goodbye..."
+
+    txt = 'scripts.outro.__poc__[3]'
+    send_sms phone_no, txt, 'goodbye'
   end
 
 end 
+
+
+
 
---
+++ lib/sms_sequence_scripts/10.rb
@@ -0,0 +1,14 @@
+Birdv::DSL::ScriptClient.new_script 'day10', 'sms' do
+  sequence 'firstmessage' do |phone_no|
+    txt = 'scripts.intro_sms.__poc__[1]'
+    puts "sending intro txt..."
+    send_sms phone_no, txt, 'firstmessage', 'image1'
+  end
+
+  sequence 'image1' do |phone_no|
+    img = 'mms.stories.moon[0]'
+    puts "sending first image..."
+    send_mms phone_no, img, 'image1'
+  end
+
+end 
