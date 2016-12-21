2c4e2866691100eceb0c763f85d93680c01d60f2
jdmcpeek <david.mcpeek@yale.edu>
2016-12-21 13:05:28 -0800

    fixed migrations for production finally

--- db/migrations/019_test.rb
+++ db/migrations/019_test.rb
@@ -3,12 +3,16 @@ Sequel.migration do
     alter_table(:state_tables) do
       add_column :last_unique_story, Integer, default: 0
       add_column :last_unique_story_read?, TrueClass, default: true
+      # drop_column :last_unique_story
+      # drop_column :last_unique_story_read?
     end
   end
   down do
     alter_table(:state_tables) do
       drop_column :last_unique_story
       drop_column :last_unique_story_read?
+      # add_column :last_unique_story, Integer, default: 0
+      # add_column :last_unique_story_read?, TrueClass, default: true
     end
   end
 end
--- test.rb
+++ test.rb
@@ -1,24 +1,22 @@
-# require_relative 'bin/test'
+require_relative 'bin/production'
 
+User.where(platform: 'fb').each do |u|
+  # if u.state_table.last_unique_story == 11
+  #   u.state_table.update(last_unique_story: 10)
+  # end
+  if u.state_table.story_number < 10
+    u.state_table.update(last_unique_story: u.state_table.story_number)
+  else
+    u.state_table.update(last_unique_story: 10)
+  end
+  puts "story_number = #{u.state_table.story_number}, last_unique_story = #{u.state_table.last_unique_story}, story_count = #{$story_count}, last_unique_story_read = #{u.state_table.last_unique_story_read?}, last_story_read= #{u.state_table.last_story_read?}"
+end
 
-
-# User.where(platform: 'fb').each do |u|
-#   if u.state_table.last_unique_story == 11
-#     u.state_table.update(last_unique_story: 10)
-#   end
-#   puts "story_number = #{u.state_table.story_number}, last_unique_story = #{u.state_table.last_unique_story}, story_count = #{$story_count}, last_unique_story_read = #{u.state_table.last_unique_story_read?}, last_story_read= #{u.state_table.last_story_read?}"
-#   # if u.state_table.story_number < $story_count
-#   #   u.state_table.update(last_unique_story: u.state_table.story_number)
-#   # else
-#   #   u.state_table.update(last_unique_story: $story_count)
-#   # end
-# end
-
-# User.where(platform: 'sms').each do |u|
-#   puts "story_number = #{u.state_table.story_number}, last_unique_story = #{u.state_table.last_unique_story}, story_count = #{$sms_story_count}, last_unique_story_read = #{u.state_table.last_unique_story_read?}, last_story_read= #{u.state_table.last_story_read?}"
-#   # if u.state_table.story_number < $sms_story_count
-#   #   u.state_table.update(last_unique_story: u.state_table.story_number)
-#   # else
-#   #   u.state_table.update(last_unique_story: $sms_story_count)
-#   # end
-# end
+User.where(platform: 'sms').each do |u|
+  if u.state_table.story_number < 10
+    u.state_table.update(last_unique_story: u.state_table.story_number)
+  else
+    u.state_table.update(last_unique_story: 10)
+  end
+  puts "story_number = #{u.state_table.story_number}, last_unique_story = #{u.state_table.last_unique_story}, story_count = #{$sms_story_count}, last_unique_story_read = #{u.state_table.last_unique_story_read?}, last_story_read= #{u.state_table.last_story_read?}"
+end
