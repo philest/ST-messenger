15132ab91b94744bad8fff033a41030f86d53723
jdmcpeek <david.mcpeek@yale.edu>
2017-02-12 01:54:37 -0500

    can query for events between time ranges. now on to sorting by timestamp

--- test.rb
+++ test.rb
@@ -1,58 +1,161 @@
-require 'httparty'
-require_relative 'config/initializers/aws'
-require_relative 'bin/local'
-require_relative 'lib/workers'
+# succeed in downloading the dev project's data in whatever available format
 
-s = School.where(signature: "ST Prep").first
-t = Teacher.where(email: "josedmcpeek@gmail.com").first
-t.update(notified_on: Time.now - 1.week)
-# t.signup_user(User.create(first_name: "David"))
-# t.signup_user(User.create(first_name: "Aubrey", last_name: "Wahl"))
-# t.signup_user(User.create(first_name: "Phil"))
-t.signup_user(User.create())
-t.signup_user(User.create())
-t.signup_user(User.create())
-t.signup_user(User.create())
+# https://mixpanel.com/api/2.0/segmentation?from_date=2017-02-8&to_date=2017-02-11&event=Viewed%20Page
 
+# https://edbe89118bef0fa1f497d5db9c6b9ab5@mixpanel.com/api/2.0/segmentation?from_date=2016-02-11&to_date=2016-02-11&event=Phone Entered
 
-# 1. no names
-# 2. only names
-# 3. both names and unnamed
+# curl https://mixpanel.com/api/2.0/segmentation/ -u edbe89118bef0fa1f497d5db9c6b9ab5: -d from_date="2017-02-08" -d to_date="2017-02-11" -d event="Phone Entered"
+
+# RAW DUMP:
 # 
+# curl 'https://data.mixpanel.com/api/2.0/export/?from_date=2017-02-08&to_date=2017-02-11&event=["Sync+Completely+Succeeded"]' -u edbe89118bef0fa1f497d5db9c6b9ab5:
 
-require_relative 'lib/workers/notify_teacher_worker'
 
-# require 'createsend'
 
-# # Authenticate with your API key
-# auth = { :api_key => '3178e57316547310895b48c195da986ee9d65a2bab76724d' }
+# alright, I'm fine with this algorithm for now. let me get some food, mull it over,
+# and if i can't think of anything better or wrong with it, let's implement!
+# sort all entrance/exit events by timestamp (entrance/exits)
+# iterate through list
+# 
+# if hit entrance on page X, store start in variable for that page
+#   keep iterating
+#   if you hit the exit on page X,
+#     record entrance - exit timestamps
+#     close the timestamp
+#     
+#   if you hit another entrance for a different page, just open interval for that page
+#   
+#   if you hit a second entrance for SAME page (X), reset the timestamp to that entrance
+#     because something got fucked up along the way
+#     as in, you shouldn't enter a page, then enter again! that is fucked
 
-# # The unique identifier for this smart email
-# smart_email_id = '98b9048d-a381-445e-8d21-65a3a5cb2b37'
+require 'httparty'
+require 'active_support/time'
 
-# # Create a new mailer and define your message
-# tx_smart_mailer = CreateSend::Transactional::SmartEmail.new(auth, smart_email_id)
-# message = {
-#   'To' => 'Phil Esterman <david@joinstorytime.com>',
-#   'Data' => {
-#     'x-apple-data-detectors' => 'x-apple-data-detectorsTestValue',
-#     'href^="tel"' => 'href^="tel"TestValue',
-#     'href^="sms"' => 'href^="sms"TestValue',
-#     'owa' => 'owaTestValue',
-#     'family_count' => 'family_countTestValue',
-#     'family_or_families' => 'family_or_familiesTestValue',
-#     'list_of_families' => 'list_of_familiesTestValue',
-#     'quicklink' => 'quicklinkTestValue',
-#     'signature' => 'signatureTestValue'
+
+class ProcessMixpanel
+  # separate class for all the processing
+end
+
+
+
+class Mixpanel
+  include HTTParty
+  base_uri 'https://data.mixpanel.com/api/2.0/export'
+
+  @@default_range = {
+    from_date: (Time.now-2.days),
+    to_date: (Time.now-1.day)
+  }
+
+  def initialize(secret_key="edbe89118bef0fa1f497d5db9c6b9ab5")
+    @auth = { username: secret_key, password: '' }
+  end
+
+  # add helper methods like converting dates to the proper format
+  def events(options = @@default_range)
+    # format dates
+    options[:from_date] = format_date(options[:from_date]) if !options[:from_date].nil?
+    options[:to_date] = format_date(options[:to_date]) if !options[:to_date].nil?
+
+    res = self.class.get('', query: options, basic_auth: @auth)
+    puts res
+
+    if res.response.code.to_i == 200
+      res.body.each_line.map {|line| JSON.parse line }
+    end
+  end
+
+  def sort_by_timestamp(events)
+    # get dates from events
+    # sort that shit
+  end
+
+  def total_session_time(event1, event2)
+
+  end
+
+
+  private
+  def format_date(date)
+    date.strftime("%Y-%m-%d")
+  end
+
+
+end
+
+
+
+# entering view
+# {
+#   event: "story page entered",
+#   timestamp: Time.now,
+#   properties: {
+#     story: "Penguin",
+#     page: 3
 #   }
 # }
 
-# # Send the message and save the response
-# response = tx_smart_mailer.send(message)
+# # exiting view
+# {
+#   event: "story page exited",
+#   timestamp: Time.now + 1.minute, # or something
+#   properties: {
+#     story: "Penguin",
+#     page: 3
+#   }
+# }
 
 
-worker = NotifyTeacherWorker.new
 
-worker.new_users_notification(t)
+# 
 
 
+
+# why would an entrance or exit fail to track? or handle 
+# 
+# two entering views before an exiting view
+# 
+# expect: e1, x1, e2, x2
+# got:    e1, e2, x1, x2
+# 
+# e1, e2, x1, e3, x2, e3
+# 
+# 
+# 
+# we can possibly sort the events by entrance/exit and choose the closest ones
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+
+
+
+
+
+
+# # The API also supports passing a time interval rather than an explicit date range
+# data = client.request(
+#   'events/properties',
+#   event:    'splash features',
+#   name:     'feature',
+#   values:   '["uno", "dos"]',
+#   type:     'unique',
+#   unit:     'day',
+#   interval: 7,
+#   limit:    5
+# )
+
+# # Use the import API, which allows one to specify a time in the past, unlike the track API.
+# # note that you need to include your api token in the data. More details at:
+# # https://mixpanel.com/docs/api-documentation/importing-events-older-than-31-days
+# data_to_import = {'event' => 'firstLogin', 'properties' => {'distinct_id' => guid, 'time' => time_as_integer_seconds_since_epoch, 'token' => api_token}}
+# require 'base64' # co-located with the Base64 call below for clarity
+# encoded_data = Base64.encode64(data_to_import.to_json)
+# data = client.request('import', {:data => encoded_data, :api_key => api_key})
+# # data == [1] # => true # you can only import one event at a time
+# # 
+# # 
--- test.sh
+++ test.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
 
-# This script just pushes the current branch to its remote counterpart on github. 
-# It by no means deploys the code. Shocking? You'll live. 
-
-[ -s "$HOME/.rvm/scripts/rvm" ] && . "$HOME/.rvm/scripts/rvm"
-echo "Removing Gemfile.lock..."
-rm Gemfile.lock
-echo "Switching to JRuby..."
-rvm use jruby@birdv
-echo "Bundle install..."
-bundle install
-branch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
-echo "Committing current version to git..."
-git add --all
-read -p "commit message: " commit_message
-git commit -m "${commit_message}"
-echo "Pushing to 'origin-${branch}'"
-git push origin $branch
-echo "Done."
-
+curl -g 'https://data.mixpanel.com/api/2.0/export/?from_date=2017-02-08&to_date=2017-02-11&event=["Phone Entered"]' -u edbe89118bef0fa1f497d5db9c6b9ab5:
