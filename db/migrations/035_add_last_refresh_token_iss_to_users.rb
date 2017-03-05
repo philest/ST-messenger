761ea548f99c184e889c957e8c470df31859c51f
Aubrey Wahl <aawahl@gmail.com>
2017-03-04 21:50:58 -0500

    forgot to add specs and new migration

---
+++ db/migrations/035_add_last_refresh_token_iss_to_users.rb
@@ -0,0 +1,12 @@
+Sequel.migration do
+  up do
+    alter_table(:users) do
+      add_column :last_refresh_token_iss, DateTime, default: DateTime.parse("2017-02-01T23:00:00.000000000+0000")
+    end
+  end
+  down do
+    alter_table(:users) do
+      drop_column :last_refresh_token_iss
+    end
+  end
+end
